03d40cdcc80231ee0aef05f7f6757e4c54801328
Strong-Foundation <179532304+Strong-Foundation@users.noreply.github.com>
2025-06-28 10:05:47 -0400

    updated

--- main.go
+++ main.go
@@ -51,15 +51,15 @@ func main() {
 func appendAndWriteToFile(path string, content string) {
 	filePath, err := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
-		log.Fatalln(err)
+		log.Println(err)
 	}
 	_, err = filePath.WriteString(content + "\n")
 	if err != nil {
-		log.Fatalln(err)
+		log.Println(err)
 	}
 	err = filePath.Close()
 	if err != nil {
-		log.Fatalln(err)
+		log.Println(err)
 	}
 }
 
